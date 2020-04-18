b94ec55f57bbf0f74c1a5dde2319c02b9f51c1ca
Jessica K <jessikim807@gmail.com>
2020-04-18 18:57:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,13 +35,10 @@ end
 
 def hit?
   prompt_user
-  user_input = get_user_input
-  if _user_input == 'h'
+  if get_user_input == 'h'
     card_total += deal_card
-  elsif user_input == 's'
-    card_total
-  else
-    invalid_command
+  end
+  card_total
   end# code hit? here
 end
 
